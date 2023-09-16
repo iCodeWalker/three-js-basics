321123a7d2dec150da0f1955fa6695f52f8ac0c6
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-09-16 14:32:42 +0530

    Added : custom canvas to renderer

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -11,8 +12,18 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.z = 2;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+const canvas = document.getElementById("c1") as HTMLCanvasElement;
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(200, 200); // hardcoding width and height
+
+{
+  /*dynamically adds renderer to document body
+creates a canvas dynamically */
+}
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
 
 new OrbitControls(camera, renderer.domElement); // can interact using mouse now
@@ -26,6 +37,7 @@ const material = new THREE.MeshBasicMaterial({
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+// Event handler to listen for resize of screen
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
