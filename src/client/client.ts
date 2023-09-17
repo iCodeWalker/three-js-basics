d8a6470dc18c01af89553478611d5c51d76a089a
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-09-17 18:43:43 +0530

    Added : stats and gui

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "dat.gui";
 
 const scene = new THREE.Scene();
 
@@ -37,6 +39,28 @@ function onWindowResize() {
   render(); // This render will re-create the scene on window resize
 }
 
+const stats = new Stats(); // use Stats module
+document.body.append(stats.dom); // add stats to document body
+
+// we need to use animate function as the stats panel will not render on it's own
+
+const gui = new GUI();
+const cubeFolder = gui.addFolder("Cube");
+cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+cubeFolder.open();
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "z", 0, 20);
+cameraFolder.open();
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  render();
+  stats.update();
+}
+
 // function animate() {
 //   requestAnimationFrame(animate);
 
@@ -50,11 +74,12 @@ function render() {
   renderer.render(scene, camera);
 }
 
-// animate();
+animate();
 render();
 
-// we can optimize our app so that our scene doesn't get re-created every second.
+//------> we can optimize our app so that our scene doesn't get re-created every second.
 
-// 1. we can use orbital-control. add change event Listener to it, that calls render. This controls mouse drag on scene
+// 1. we can use orbital-control for on demand rendering. add change event Listener to it,
+//  that calls render. This controls mouse drag on scene.
 // 2. we can call render() only one time.
 // 3. we can add render() to resize event listener, so our scene gets re-created when window size changes.
