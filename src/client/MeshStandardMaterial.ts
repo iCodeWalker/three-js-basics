378a98ef322edfc764528a900583bf264604bb84
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-09-26 23:49:11 +0530

    Added : MeshPhysicalMaterial

--- src/client/MeshStandardMaterial.ts
+++ src/client/MeshStandardMaterial.ts
@@ -30,47 +30,32 @@ const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0);
 const planeGeometry = new THREE.PlaneGeometry();
 const torusKnotGeometry = new THREE.TorusKnotGeometry();
 
-const material = new THREE.MeshPhysicalMaterial({});
-material.reflectivity = 0;
-material.transmission = 1.0;
-material.roughness = 0.2;
-material.metalness = 0;
-material.clearcoat = 0.3;
-material.clearcoatRoughness = 0.25;
-material.color = new THREE.Color(0xffffff);
-material.ior = 1.2;
-material.thickness = 10.0;
+// It uses the Physically Based Rendering (PBR) model.
+
+// It creates a more realistic appearance than the MeshLambertMaterial or the MeshPhongMaterial.
+// It is also more computationally expensive.
+const material = new THREE.MeshStandardMaterial();
 
 // const texture = new THREE.TextureLoader().load('img/grid.png')
 // material.map = texture
-
-const pmremGenerator = new THREE.PMREMGenerator(renderer);
-const envTexture = new THREE.CubeTextureLoader().load(
-  [
-    "img/px_50.png",
-    "img/nx_50.png",
-    "img/py_50.png",
-    "img/ny_50.png",
-    "img/pz_50.png",
-    "img/nz_50.png",
-  ],
-  () => {
-    material.envMap = pmremGenerator.fromCubemap(envTexture).texture;
-    pmremGenerator.dispose();
-    scene.background = material.envMap;
-  }
-);
+// const pmremGenerator = new THREE.PMREMGenerator(renderer)
+// const envTexture = new THREE.CubeTextureLoader().load(['img/px_50.png','img/nx_50.png','img/py_50.png','img/ny_50.png','img/pz_50.png','img/nz_50.png'],
+//     () => {
+//         material.envMap = pmremGenerator.fromCubemap(envTexture).texture
+//         pmremGenerator.dispose()
+//     }
+// )
 
 const cube = new THREE.Mesh(boxGeometry, material);
 cube.position.x = 5;
 scene.add(cube);
 
 const sphere = new THREE.Mesh(sphereGeometry, material);
-sphere.position.x = 0;
+sphere.position.x = 3;
 scene.add(sphere);
 
 const icosahedron = new THREE.Mesh(icosahedronGeometry, material);
-icosahedron.position.x = 3;
+icosahedron.position.x = 0;
 scene.add(icosahedron);
 
 const plane = new THREE.Mesh(planeGeometry, material);
@@ -122,28 +107,21 @@ const data = {
   emissive: material.emissive.getHex(),
 };
 
-const meshPhysicalMaterialFolder = gui.addFolder("THREE.MeshPhysicalMaterial");
+const meshStandardMaterialFolder = gui.addFolder("THREE.MeshStandardMaterial");
 
-meshPhysicalMaterialFolder.addColor(data, "color").onChange(() => {
+meshStandardMaterialFolder.addColor(data, "color").onChange(() => {
   material.color.setHex(Number(data.color.toString().replace("#", "0x")));
 });
-meshPhysicalMaterialFolder.addColor(data, "emissive").onChange(() => {
+meshStandardMaterialFolder.addColor(data, "emissive").onChange(() => {
   material.emissive.setHex(Number(data.emissive.toString().replace("#", "0x")));
 });
-
-meshPhysicalMaterialFolder.add(material, "wireframe");
-meshPhysicalMaterialFolder
+meshStandardMaterialFolder.add(material, "wireframe");
+meshStandardMaterialFolder
   .add(material, "flatShading")
   .onChange(() => updateMaterial());
-meshPhysicalMaterialFolder.add(material, "reflectivity", 0, 1);
-meshPhysicalMaterialFolder.add(material, "roughness", 0, 1);
-meshPhysicalMaterialFolder.add(material, "metalness", 0, 1);
-meshPhysicalMaterialFolder.add(material, "clearcoat", 0, 1, 0.01);
-meshPhysicalMaterialFolder.add(material, "clearcoatRoughness", 0, 1, 0.01);
-meshPhysicalMaterialFolder.add(material, "transmission", 0, 1, 0.01);
-meshPhysicalMaterialFolder.add(material, "ior", 1.0, 2.333);
-meshPhysicalMaterialFolder.add(material, "thickness", 0, 10.0);
-meshPhysicalMaterialFolder.open();
+//meshStandardMaterialFolder.add(material, 'roughness', 0, 1)
+//meshStandardMaterialFolder.add(material, 'metalness', 0, 1)
+meshStandardMaterialFolder.open();
 
 function updateMaterial() {
   material.side = Number(material.side) as THREE.Side;
@@ -152,9 +130,6 @@ function updateMaterial() {
 
 function animate() {
   requestAnimationFrame(animate);
-
-  // torusKnot.rotation.x += 0.01
-  // torusKnot.rotation.y += 0.01
 
   render();
 
