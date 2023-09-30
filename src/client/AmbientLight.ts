fbe7bf1c630d7fe55ff969349f5ca821fe4d4fff
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-09-30 14:42:34 +0530

    Added : Ambient light

--- src/client/AmbientLight.ts
+++ src/client/AmbientLight.ts
@@ -1,3 +1,34 @@
+// There are various kinds of lights in Threejs.
+
+// They all extend from the THREE.Light base class, which in turn also extends from the Object3D base class.
+
+// The base class properties
+
+// color
+// intensity
+// isLight (Read Only)
+// Lighting gives you many more options to change the appearance of meshes within the scene.
+// Meshes will need materials added to them in order for the lighting adjustments to take effect.
+
+// If a scene has no lighting, most materials won't be visible.
+// The MeshBasicMaterial, MeshNormalMaterial and the MeshMatcapMaterial are self illuminating,
+// so they don't need lighting to be visible within a scene, but most of the other materials do,
+// such as the MeshLambertMaterial, MeshPhongMaterial, MeshStandardMaterial, MeshPhysicalMaterial
+// and MeshToonMaterial.
+
+// ----------- Ambient Light -------------
+
+// 1. Lights all objects in the scene equally, except for self illuminating objects such as
+// MeshBasicMaterial, MeshNormalMaterial and MeshMatcapMaterial.
+
+// 2. Doesn't cast shadows.
+
+// 3. Light is spread equally in all directions and distances. So positioning the light different from
+// the default position of [0, 0, 0] will make no difference.
+
+// 4. Materials won't show shading depending on geometry normals and there will be no specular affect,
+// so meshes in front of other meshes will be invisible if they have identical materials or even a single colour map texture.
+
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module";
@@ -23,11 +54,11 @@ document.body.appendChild(renderer.domElement);
 
 new OrbitControls(camera, renderer.domElement);
 
-const planeGeometry = new THREE.PlaneGeometry(20, 10); //, 360, 180)
-const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial());
-plane.rotateX(-Math.PI / 2);
-//plane.position.y = -1.75
-scene.add(plane);
+// const planeGeometry = new THREE.PlaneGeometry(20, 10)//, 360, 180)
+// const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
+// plane.rotateX(-Math.PI / 2)
+// //plane.position.y = -1.75
+// scene.add(plane)
 
 const torusGeometry = [
   new THREE.TorusGeometry(),
