27c282ffc66541158a7319ef558d88335a58659a
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-10-02 20:04:23 +0530

    added : directional light

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,10 +4,13 @@ import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "dat.gui";
 
 const scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper(Math.PI));
+scene.add(new THREE.AxesHelper(5));
 
-const light = new THREE.AmbientLight(0xffffff, Math.PI);
+const light = new THREE.DirectionalLight(0xffffff, Math.PI);
 scene.add(light);
+
+const helper = new THREE.DirectionalLightHelper(light);
+scene.add(helper);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -23,11 +26,11 @@ document.body.appendChild(renderer.domElement);
 
 new OrbitControls(camera, renderer.domElement);
 
-const planeGeometry = new THREE.PlaneGeometry(20, 10); //, 360, 180)
-const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial());
-plane.rotateX(-Math.PI / 2);
-//plane.position.y = -1.75
-scene.add(plane);
+// const planeGeometry = new THREE.PlaneGeometry(20, 10)//, 360, 180)
+// const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
+// plane.rotateX(-Math.PI / 2)
+// //plane.position.y = -1.75
+// scene.add(plane)
 
 const torusGeometry = [
   new THREE.TorusGeometry(),
@@ -95,8 +98,11 @@ lightFolder.addColor(data, "color").onChange(() => {
 });
 lightFolder.add(light, "intensity", 0, Math.PI * 2, 0.01);
 
-const ambientLightFolder = gui.addFolder("THREE.AmbientLight");
-ambientLightFolder.open();
+const directionalLightFolder = gui.addFolder("THREE.DirectionalLight");
+directionalLightFolder.add(light.position, "x", -100, 100, 0.01);
+directionalLightFolder.add(light.position, "y", -100, 100, 0.01);
+directionalLightFolder.add(light.position, "z", -100, 100, 0.01);
+directionalLightFolder.open();
 
 const meshesFolder = gui.addFolder("Meshes");
 meshesFolder.add(data, "mapsEnabled").onChange(() => {
@@ -112,6 +118,8 @@ meshesFolder.add(data, "mapsEnabled").onChange(() => {
 
 function animate() {
   requestAnimationFrame(animate);
+
+  //helper.update()
 
   torus.forEach((t) => {
     t.rotation.y += 0.01;
