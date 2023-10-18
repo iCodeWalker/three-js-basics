72dd804bb7fbb170b06c940b95f878d8233a8f41
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-10-18 08:32:58 +0530

    added : fbx loader

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,14 +1,22 @@
-// adding a texture to the glTF model and modifying its UV coordinates.
-// By default, the texture is embedded into the model when you export it from blender.
-// This makes it easier to create and use textured models versus when doing it using OBJ and MTL.
+// The FBX format is used to provide interoperability between digital content
+// creation applications and game engines such as Blender, Maya, Autodesk, Unity, Unreal
+// and many others. It supports many features such as 3D models, scene hierarchy, materials,
+// lighting, animations, bones and more.
 
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import Stats from "three/examples/jsm/libs/stats.module";
 
 const scene = new THREE.Scene();
 scene.add(new THREE.AxesHelper(5));
+
+const light = new THREE.PointLight(0xffffff, 50);
+light.position.set(0.8, 1.4, 1.0);
+scene.add(light);
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -16,37 +24,38 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 2;
+camera.position.set(0.8, 1.4, 1.0);
 
 const renderer = new THREE.WebGLRenderer();
-//renderer.physicallyCorrectLights = true //deprecated
-renderer.useLegacyLights = false; //use this instead of setting physicallyCorrectLights=true property
-renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
+controls.target.set(0, 1, 0);
 
-const loader = new GLTFLoader();
-loader.load(
-  "models/monkey_textured.glb",
-  function (gltf) {
-    gltf.scene.traverse(function (child) {
+// const material = new THREE.MeshNormalMaterial();
+
+const fbxLoader = new FBXLoader();
+fbxLoader.load(
+  "models/Kachujin G Rosales.fbx",
+  (object) => {
+    // making our object opaque as it was previously transparent
+    object.traverse(function (child) {
+      console.log(child.name); // we can use this to change different things like cast shadow etc on objects
       if ((child as THREE.Mesh).isMesh) {
-        const m = child as THREE.Mesh;
-        m.receiveShadow = true;
-        m.castShadow = true;
-      }
-      if ((child as THREE.Light).isLight) {
-        const l = child as THREE.SpotLight;
-        l.castShadow = true;
-        l.shadow.bias = -0.003;
-        l.shadow.mapSize.width = 2048;
-        l.shadow.mapSize.height = 2048;
+        // we can also remove the material completely
+        // (child as THREE.Mesh).material = material;
+        if ((child as THREE.Mesh).material) {
+          (
+            (child as THREE.Mesh).material as THREE.MeshBasicMaterial
+          ).transparent = false;
+        }
       }
     });
-    scene.add(gltf.scene);
+    // scaling the object as in fbx one unit is 1 cm, so our model is 100 times larger as our 1 unit is 1 m
+    object.scale.set(0.01, 0.01, 0.01);
+    scene.add(object);
   },
   (xhr) => {
     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
